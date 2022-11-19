4c83f2f84def5558717d660fcce90abd203a877e
RenatDavletshin <immortal1307@gmail.com>
2022-11-19 19:19:19 +0500

    FirstTaskEnd

---
+++ FirstTask/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввел пользователь.
+
+Console.Write($"Введите число М(количество чисел): ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[M];
+
+void InputNumbers(int M)
+{
+for (int i = 0; i < M; i++)
+  {
+    Console.Write($"Введи {i+1} число: ");
+    massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if (massiveNumbers[i] > 0) 
+    count++; 
+  }
+  return count;
+}
+
+InputNumbers(M);
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
